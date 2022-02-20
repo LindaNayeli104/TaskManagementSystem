22b8e405efe56764a7d636d5a81165d0e6337d2c
LindaNayeli104 <linda_nayeli104@hotmail.com>
2022-02-20 13:25:29 -0600

    fix: is_completed attribute match with edit interface info

--- editTask.js
+++ editTask.js
@@ -11,9 +11,9 @@ const dueDate = document.querySelector("#task-due-date");
 const comments = document.querySelector("#task-comments");
 const description = document.querySelector("#task-description");
 const tags = document.querySelector("#task-tags");
-//const isCompleted = 0;
+const isCompleted = document.querySelector("#task-checkmark");
 
-console.log(title);
+console.log(isCompleted.checked);
 document.addEventListener("DOMContentLoaded", () => {
   let url = "https://ecsdevapi.nextline.mx/vdev/tasks-challenge/tasks";
   let urlComplement = "/" + idToPass + "?token=nextLine123456";
@@ -43,19 +43,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function renderTaks(tasks) {
     const task_title = tasks[0].title;
-    console.log(title);
     const task_dueDate = tasks[0].due_date;
     const task_comments = tasks[0].comments;
     const task_description = tasks[0].description;
     const task_tags = tasks[0].tags;
 
-    const isCompleted = tasks[0].is_completed;
+    const is_completed = tasks[0].is_completed;
 
     title.value = task_title;
     dueDate.value = task_dueDate;
     comments.value = task_comments;
     description.value = task_description;
     tags.value = task_tags;
+
+    if (is_completed) {
+      isCompleted.checked = true;
+    } else {
+      isCompleted.checked = false;
+    }
     //isCompleted.value = task_title;
   }
 });
